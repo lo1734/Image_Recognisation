8dadd4973e5ae6840e4a070670f4229c3d4b323e
Lohitaksha <112050224+lo1734@users.noreply.github.com>
2024-09-08 09:01:19 +0530

    Add files via upload

---
+++ server/server.py
@@ -0,0 +1,10 @@
+from flask import Flask,jsonify,request
+
+app=Flask(__name__)
+
+@app.route('/classify_image',methods=['GET','POST'])
+def classify_image():
+    return "hi"
+if __name__  == "__main__":
+    app.run(port=5000)
+
